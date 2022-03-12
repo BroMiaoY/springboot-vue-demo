4f6a8f725b273da9d7f6efc930df061ea65a36f6
苗苗 <2304653544@qq.com>
2022-03-13 00:39:23 +0800

    第一次

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,96 @@
+<template>
+  <div style="padding: 10px">
+    <div style="margin: 10px 0">
+      <el-button type="primary">新增</el-button>
+      <el-button type="primary">导入</el-button>
+      <el-button type="primary">导出</el-button>
+    </div>
+
+    <div style="margin: 10px 0">
+      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%"/>
+      <el-button type="primary" style="margin-left: 10px">查询</el-button>
+    </div>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date"
+                       sortable
+                       label="Date" />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="address" label="Address" />
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template #default>
+          <el-popconfirm title="确认删除吗?" >
+            <template #reference>
+              <el-button type="text">删除</el-button>
+            </template>
+          </el-popconfirm>
+          <el-button type="text" >编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div style="margin: 10px 0">
+      <el-pagination
+          :currentPage="currentPage"
+          :page-size="10"
+          :page-sizes="[5, 10, 20]"
+          :small="small"
+          :disabled="disabled"
+          :background="background"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  },
+  currentPage: 1,
+  total:10,
+  data() {
+    return {
+      search: '',
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+      ]
+    }
+  },
+  methods: {
+    handleEdit() {
+
+    },
+    handleSizeChange() {
+
+    },
+    handleCurrentChange() {
+
+    },
+  }
+}
+</script>

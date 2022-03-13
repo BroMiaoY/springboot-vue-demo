718db159dd20e6fd69537cd9523897bfd942735f
苗苗 <2304653544@qq.com>
2022-03-13 15:18:01 +0800

    第二次

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="padding: 10px">
     <div style="margin: 10px 0">
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary" @click="add">新增</el-button>
       <el-button type="primary">导入</el-button>
       <el-button type="primary">导出</el-button>
     </div>
@@ -11,11 +11,14 @@
       <el-button type="primary" style="margin-left: 10px">查询</el-button>
     </div>
     <el-table :data="tableData" stripe style="width: 100%">
-      <el-table-column prop="date"
+      <el-table-column prop="id"
                        sortable
-                       label="Date" />
-      <el-table-column prop="name" label="Name" />
-      <el-table-column prop="address" label="Address" />
+                       label="ID" />
+      <el-table-column prop="username" label="用户名" />
+      <el-table-column prop="nickName" label="昵称" />
+      <el-table-column prop="age" label="年龄" />
+      <el-table-column prop="sex" label="性别" />
+      <el-table-column prop="address" label="地址" />
       <el-table-column fixed="right" label="Operations" width="120">
         <template #default>
           <el-popconfirm title="确认删除吗?" >
@@ -54,6 +57,34 @@
 <!--          @size-change="handleSizeChange"-->
 <!--          @current-change="handleCurrentChange">-->
 <!--      </el-pagination>-->
+
+      <el-dialog v-model="dialogVisible" title="提示" width="30%">
+        <el-form :modle="form" label-width="120px">
+          <el-form-item label="用户名">
+            <el-input v-model="form.username" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="昵称">
+            <el-input v-model="form.nickName" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="年龄">
+            <el-input v-model="form.age" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-radio v-model="form.sex" label="男">男</el-radio>
+            <el-radio v-model="form.sex" label="女">女</el-radio>
+            <el-radio v-model="form.sex" label="未知">未知</el-radio>
+          </el-form-item>
+          <el-form-item label="地址">
+            <el-input type="textarea" v-model="form.address" style="width: 80%"></el-input>
+          </el-form-item>
+        </el-form>
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button @click="dialogVisible = false">取消</el-button>
+            <el-button type="primary" @click="save">确认</el-button>
+          </span>
+        </template>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -69,32 +100,22 @@ export default {
   total:10,
   data() {
     return {
+      form: {},
+      dialogVisible: false,
       search: '',
       tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
+
       ]
     }
   },
   methods: {
+    add() {
+      this.dialogVisible = true
+      this.form = {}
+    },
+    save() {
+
+    },
     handleEdit() {
 
     },
